2c6582451d907d5d047b28772824528c8cc63f1f
Nagavenkat Adurthi <n.adurthi@gmail.com>
2018-08-23 17:20:59 -0600

    completed

--- src/base.cpp
+++ src/base.cpp
@@ -5,9 +5,6 @@
 
 int div(V v){
 	v.H.push_back(3);
-	A c;
-	add(c);
-	
 	std::cout << "mvpoly OK" <<"\n";
 }
 
--- test/src/test11.cpp
+++ test/src/test11.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include "mvpoly/base.h"
-#include "UQ/first.h"
+#include "myproject/base.h"
+
 
 
 
 
 int main(int argc, char * argv[]){
 
-	A c;
-	add(c);
 	V v;
 	div(v);
-	std::cout << "ok \n";
+	std::cout << "test ok \n";
 
 }
